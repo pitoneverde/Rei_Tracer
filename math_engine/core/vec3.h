093ac245d729a7c43e8106bac744d889614c550a
pitoneverde <pitoneverde181@gmail.com>
2026-01-22 01:59:23 +0100

    update diario

--- math_engine/core/vec3.h
+++ math_engine/core/vec3.h
@@ -1,6 +1,27 @@
 #ifndef VEC3_H
 # define VEC3_H
 
+// point difference = vector
+// point + vector = point
+// vector sum = vector
+// vector difference (= -sum)
+// vector dot product
+// vector scalar multiplication/division (scale)
+// vector cross product
+// vector triple product (scalar & vectorial)
+// vector length && lenght_squared (avoid sqrt)
+// unit vectors constants
+// vector is parallel
+// vector is perpendicular
+// decompose vector over another
+// project vector over another
+// vector normalize
+// vector equals
+// point distance
+// vector lerp/slerp (do last)
 
+// ------------bonus------------
+// angle between vectors (needed later for bonus)
+// reflection/refraction vectors
 
 #endif
