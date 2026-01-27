c252d42d7f5bd9e46a715f21679c37fef1429576
pitoneverde <pitoneverde181@gmail.com>
2026-01-27 14:36:36 +0100

    gitignore

--- math_engine/config.mk
+++ math_engine/config.mk
@@ -16,7 +16,7 @@ BIN_DIR     := $(BUILD_DIR)/bin
 CC          := cc
 # compile with -ffast-math and -march=native to get max math speed
 CFLAGS      := -Wall -Wextra -Werror -std=c99 -O0 -g -march=native #-pedantic #-ffast-math
-INCLUDES    := -I$(INCLUDE_DIR) -I$(INCLUDE_MINIRT_DIR)
+INCLUDES    := -I$(INCLUDE_DIR) #-I$(INCLUDE_MINIRT_DIR)
 LDFLAGS		:= -lm
 AR          := ar
 ARFLAGS     := rcs
