aeb8abe9c43c3c698ff14b988b1744961bfca4f4
Giovanni Mu <gmu@c1r1p5.42firenze.it>
2026-03-31 15:18:09 +0200

    norminette

--- include/parsing_minirt.h
+++ include/parsing_minirt.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_minirt.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 15:16:05 by gmu               #+#    #+#             */
+/*   Updated: 2026/03/31 15:16:12 by gmu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PARSING_MINIRT_H
 # define PARSING_MINIRT_H 1
 
@@ -10,7 +22,7 @@ double		convert_double_strtod(char *str);
 bool		check_coordinates(char *s);
 bool		check_normalizzation_limits(char *str);
 
-//funzioni chiamate
+// funzioni chiamate
 bool		check_ambient(char *str);
 bool		check_camera(char *str);
 bool		check_light(char *str);
@@ -18,21 +30,21 @@ bool		check_sphere(char *str);
 bool		check_plane(char *str);
 bool		check_cylinder(char *str);
 
-void	print_debug_matrix(char **matrix);
-int	matrix_size(char **matrix);
-double	convert_double_strtod(char *str);
-bool	convertable_double(double *value, const char *str);
+void		print_debug_matrix(char **matrix);
+int			matrix_size(char **matrix);
+double		convert_double_strtod(char *str);
+bool		convertable_double(double *value, const char *str);
 
-int	check_split(char *s1);
-int	matrix_strlen_check(char **matrix);
-char	*read_from_file(int fd);
-char	**matrix_compress(char **matrix);
-bool	matrix_compress_app(char **matrix, char **new_matrix);
-char	*compress_space(char *str);
-int	count_non_empty_lines(char **matrix);
-int	is_empty_lines(char *str);
+int			check_split(char *s1);
+int			matrix_strlen_check(char **matrix);
+char		*read_from_file(int fd);
+char		**matrix_compress(char **matrix);
+bool		matrix_compress_app(char **matrix, char **new_matrix);
+char		*compress_space(char *str);
+int			count_non_empty_lines(char **matrix);
+int			is_empty_lines(char *str);
 // funzione che chiama le funzioni sopra e controlla riga per riga
-// questa e' quella da modificare nel caso volessi aggiungere altre forme 
+// questa e' quella da modificare nel caso volessi aggiungere altre forme
 // geometriche, per farlo aggiungi una funzione nella cascata degli if else
 // le funzioni che stanno sopra controllano il formato riga per riga
 // se realmente non e' chiaro ancora cosa fanno usa il printf
@@ -47,19 +59,19 @@ bool		check_matrix_data_is_good(char **matrix);
 // e la lunghezza minima della stringa
 t_element	*parse_input(int argc, char **argv);
 
-//utility per debug
+// utility per debug
 void		print_debug_matrix(char **matrix);
 
-//init dei dati
+// init dei dati
 // i dati sono gia' formattati in modo corretto
 // bisogna modificare anche questo file che infila i dati
 // in t_element nella cascata di if else
 
-void	init_data_ambient(char *s, t_element *element);
-void	init_data_camera(char *s, t_element *element);
-void	init_data_light(char *s, t_element *element);
-void	init_data_sphere(char *s, t_element *element);
-void	init_data_plane(char *s, t_element *element);
+void		init_data_ambient(char *s, t_element *element);
+void		init_data_camera(char *s, t_element *element);
+void		init_data_light(char *s, t_element *element);
+void		init_data_sphere(char *s, t_element *element);
+void		init_data_plane(char *s, t_element *element);
 t_element	*init_data_minirt(char *s);
 
 #endif
